44943c07567251a4a974e52ecded0a623ba70d54
Armin Yaghoubi <arminyaghoubi1@gmail.com>
2022-03-23 19:16:43 +0430

    Add project files.

---
+++ MvcMovie/Migrations/20220323141619_ChangedFieldInMovie.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace MvcMovie.Migrations
+{
+    public partial class ChangedFieldInMovie : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Ganre",
+                table: "Movie",
+                newName: "Genre");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Genre",
+                table: "Movie",
+                newName: "Ganre");
+        }
+    }
+}
