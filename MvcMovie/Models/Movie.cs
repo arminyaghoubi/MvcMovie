f3b2c7d8fbcbed8d584776469c839d7b96fd79a4
Armin Yaghoubi <arminyaghoubi1@gmail.com>
2022-03-26 10:32:04 +0430

    Added Validation

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -7,13 +7,28 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int Id { get; set; }
-        public string? Title { get; set; }
+
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string Title { get; set; }
+
         [Display(Name = "Release Date")]
         [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:yyyy-MM-dd}", ApplyFormatInEditMode = true)]
         public DateTime ReleaseDate { get; set; }
-        public string? Genre { get; set; }
-        [Column(TypeName ="decimal(18,2)")]
+
+        [Required]
+        [StringLength(30)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z\s]*$")]
+        public string Genre { get; set; }
+
+        [Range(0, 100)]
+        [DataType(DataType.Currency)]
+        [Column(TypeName = "decimal(18,2)")]
         public decimal Price { get; set; }
+
+        [StringLength(5)]
+        [RegularExpression(@"^[A-Z]+[a-zA-Z0-9""'\s-]*$")]
         public string? Rating { get; set; }
     }
 }
